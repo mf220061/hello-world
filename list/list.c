1f819d02347ba4c023a67741c4c9d5c67f5e0e6d
mf220061 <mf220061@g.u-fukui.ac.jp>
2022-06-08 15:35:33 +0900

    Add delete module to list.

--- list/list.c
+++ list/list.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 struct element {
-  int value, id;
+  int value;
   struct element* next;
   struct element* prev;
 };
@@ -15,26 +15,66 @@ struct list {
   struct element* tail;
 };
 
-struct list* tail_add(struct list* lst, int value) {
+struct list* create_list(struct list* list, struct element* node) {
+  list->head = node;
+  list->tail = node;
+  return list;
+}
+
+struct list* tail_add(struct list* list, int value) {
   struct element* node = (struct element*)malloc(sizeof(struct element));
   node->value = value;
   node->next = NULL;
-  lst->n++;
-  if (lst->head == NULL) {
-    lst->head = node;
-    lst->tail = node;
-    return lst;
+  list->n++;
+  if (list->head == NULL) {
+    return create_list(list, node);
   } else {
-    struct element* p = lst->tail;
+    struct element* p = list->tail;
     p->next = node;
     node->prev = p;
-    lst->tail = node;
-    return lst;
+    list->tail = node;
+    return list;
   }
 }
 
-void show(struct list* lst) {
-  struct element* p = lst->head;
+struct list* head_add(struct list* list, int value) {
+  struct element* node = (struct element*)malloc(sizeof(struct element));
+  node->value = value;
+  node->prev = NULL;
+  list->n++;
+  if (list->head == NULL) {
+    return create_list(list, node);
+  } else {
+    struct element* p = list->head;
+    p->prev = node;
+    node->next = p;
+    list->head = node;
+    return list;
+  }
+}
+
+struct list* delete(struct list* list, int position) {
+  if (position >= list->n || position < 0) {
+    return list;
+  }
+  struct element* p = list->head;
+  for (int i = 0; i < position; i++) {
+    p = p->next;
+  }
+  struct element* q = p->next;
+  q->prev = p->prev;
+  p->prev->next = q;
+  free(p);
+}
+
+struct list* insert(struct list* list, int position) {
+  if (position >= list->n || position < 0) {
+    return list;
+  }
+}
+
+void show(struct list* list) {
+  struct element* p = list->head;
   while (p->next != NULL) {
     printf("%d ", p->value);
     p = p->next;
@@ -49,14 +89,20 @@ int get_random(int min, int max) {
 
 int main(void) {
   struct element* node = NULL;
-  struct list* lst = (struct list*)malloc(sizeof(struct list));
-  lst->n = 0;
+  struct list* list = (struct list*)malloc(sizeof(struct list));
+  list->n = 0;
 
   srand((unsigned int)time(NULL));
   for (int i = 0; i < 10; i++) {
-    lst = tail_add(lst, get_random(0, 9));
-    show(lst);
+    if (rand() % 2 == 0) {
+      list = tail_add(list, get_random(0, 9));
+    } else {
+      list = head_add(list, get_random(0, 9));
+    }
+    show(list);
   }
-  show(lst);
+  show(list);
+  delete(list, 1);
+  show(list);
   return 0;
 }
